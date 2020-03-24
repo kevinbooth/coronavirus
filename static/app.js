1143c50dc0b091d13bb0e127755333ddcebf2706
Kevin Booth <kdbooth97@gmail.com>
2020-03-23 21:24:27 -0400

    Pull in data from API and sort it

--- static/app.js
+++ static/app.js
@@ -15,11 +15,15 @@ const axios = require('axios');
  * @namespace
  */
 var COVID19 = window.COVID19 || {
-    //api: 'httpss://api.covid19api.com/summary',
-    api: 'http://coronavirus-tracker-api.herokuapp.com/v2/',
-    source: '?source=jhu',
+    api: 'https://api.covid19api.com/',
+    //api: 'http://coronavirus-tracker-api.herokuapp.com/v2/',
     latest: '',
-    locations: ''
+    confirmed: '',
+    newConfirmed: '',
+    recovered: '',
+    newRecovered: '',
+    deaths: '',
+    newDeaths: '',
 };
 
 /**
@@ -48,12 +52,47 @@ COVID19.init = function () {
  * @function
  */
 COVID19.fetch = function () {
-    axios.get(COVID19.api + 'locations' + COVID19.source)
+    axios.get(COVID19.api + 'summary')
     .then(function (response) {
-        COVID19.latest = response.data.latest;
-        console.log(COVID19.latest);
-        COVID19.locations = response.data.locations;
-        console.log(COVID19.locations);
+        let data = response.data.Countries;
+
+        COVID19.latest = data.sort(function (a, b) {
+          return b.TotalConfirmed - a.TotalConfirmed;
+        });
+
+        COVID19.confirmed = data.reduce(function(sum, d) {
+          return sum + d.TotalConfirmed;
+        }, 0);
+
+        COVID19.newConfirmed = data.reduce(function(sum, d) {
+            return sum + d.NewConfirmed;
+        }, 0);
+
+        COVID19.recovered = data.reduce(function(sum, d) {
+            return sum + d.TotalRecovered;
+        }, 0);
+
+        COVID19.newRecovered = data.reduce(function(sum, d) {
+            return sum + d.NewRecovered;
+        }, 0);
+
+        COVID19.deaths = data.reduce(function(sum, d) {
+            return sum + d.TotalDeaths;
+        }, 0);
+
+        COVID19.newDeaths = data.reduce(function(sum, d) {
+            return sum + d.NewDeaths;
+        }, 0);
+
+        console.log('raw data', response);
+        console.log('latest', COVID19.latest);
+        console.log('confirmed', COVID19.confirmed);
+        console.log('newConfirmed', COVID19.newConfirmed);
+        console.log('recovered', COVID19.recovered);
+        console.log('newRecovered', COVID19.newRecovered);
+        console.log('deaths', COVID19.deaths);
+        console.log('newDeaths', COVID19.newDeaths);
+
     }).catch(function (err) {
         console.log('error', err)
     });
